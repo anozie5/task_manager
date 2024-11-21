241f39867ae1945251513a56fdf31c1628fdd175
anozie5 <chinemeremanozie5@gmail.com>
2024-11-21 06:06:05 +0100

    here

--- work/backend/api/urls.py
+++ work/backend/api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import *
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns=[
-
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path
 ]
