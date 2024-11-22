4616f2d26ff253d514bc9d4ae52203f12cf8d5d0
anozie5 <chinemeremanozie5@gmail.com>
2024-11-22 02:43:52 +0100

    here

--- work/backend/api/urls.py
+++ work/backend/api/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import *
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+
+router=DefaultRouter()
+router.register(r'task', JobView.as_view(), basename='task')
+router.register(r'pay', PayView.as_view(), basename='pay')
 
 urlpatterns=[
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('signup/', CreateUser.as_view()),
-    path('login/', LoginUser.as_view()),
+    path('signup/', CreateUser.as_view(), name='signup'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('', include(router.urls)),
 ]
